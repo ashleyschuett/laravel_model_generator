e53f6ba3c3893e6da8c79009afd77fa592061310
Ashley Schuett <ashleyns1992@gmail.com>
2014-12-09 11:16:34 -0600

    init commit

---
+++ script.php
@@ -0,0 +1,41 @@
+<?php
+
+$args = explode(',',$argv[1]);
+
+$file = 'file.php';
+$handle = fopen($file, 'w') or die ('Connot open file: '.$file);
+
+$content = '<?php '."\n".' 
+protected $fillable=[';
+
+foreach($args as $arg){
+	$content = $content .  "'". $arg ."',";
+}
+	
+$content = $content . "] \n 
+public static function rules(){ \n 
+	return array(\n";
+
+foreach ($args as $arg){
+	$content = $content . "\t\t\t'".$arg."' => 'required',\n";
+}
+
+$content = $content . "\t)\n
+} \n
+public static function saver(){ \n
+	return array(\n";
+
+
+foreach ($args as $arg){
+	$content = $content . "\t\t\t'".$arg."' => Input::get('".$arg."')\n";
+}
+
+$content = $content . "\t)\n
+}\n
+?>";
+
+
+fwrite($handle, $content);
+fclose($handle);
+
+?>
