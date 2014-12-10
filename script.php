7afe8efa208f3ab72dfd7e6d117ed387d468d225
Ashley Schuett <ashleyns1992@gmail.com>
2014-12-10 08:57:27 -0600

    Made the script more efficient.

--- script.php
+++ script.php
@@ -5,35 +5,30 @@ $args = explode(',',$argv[1]);
 $file = 'file.php';
 $handle = fopen($file, 'w') or die ('Connot open file: '.$file);
 
-$content = '<?php '."\n".' 
-protected $fillable=[';
+
+$fillable = '';
+$rules = '';
+$saver = '';
 
 foreach($args as $arg){
-	$content = $content .  "'". $arg ."',";
+	$fillable = $fillable .  "'". $arg ."',";
+	$rules = $rules . "\t\t\t'".$arg."' => 'required',\n";
+	$saver = $saver . "\t\t\t'".$arg."' => Input::get('".$arg."'),\n";
 }
+
+$content = '<?php '."\n".' 
+	protected $fillable=['. $fillable ."];\n
 	
-$content = $content . "]; \n 
-public static function rules(){ \n 
-	return array(\n";
-
-foreach ($args as $arg){
-	$content = $content . "\t\t\t'".$arg."' => 'required',\n";
-}
-
-$content = $content . "\t);\n
-} \n
-public static function saver(){ \n
-	return array(\n";
-
-
-foreach ($args as $arg){
-	$content = $content . "\t\t\t'".$arg."' => Input::get('".$arg."'),\n";
-}
-
-$content = $content . "\t);\n
-}\n
+	public static function rules(){\n
+		return array(\n" .$rules."
+		);\n
+	}\n
+	
+	public static function saver(){ \n
+		return array(\n".$saver."
+		);\n
+	}\n
 ?>";
-
 
 fwrite($handle, $content);
 fclose($handle);
